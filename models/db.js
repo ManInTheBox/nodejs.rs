cb64f9031beff1e6b584b688462dc3bcba1d3b2d
Zarko Stankovic <stankovic.zarko@gmail.com>
2012-06-24 04:59:22 +0200

    u procesu #40. radi skripta treba je srediti do kraja

--- models/db.js
+++ models/db.js
@@ -10,20 +10,37 @@ var mongoose = require('mongoose'),
 
 /**
  * Connects to MongoDB.
+ *
+ * Connection will be automatically made after `require('./db')`.
+ * It will use connection string supplied either through stdin
+ * or `credentials.json` (default).
+ * Valid console arguments are: `-c` or `--connection-string`.
  */
 
-mongoose.connect(credentials.db);
+(function () {
+  var argv = process.argv;
+  var c = argv.indexOf('-c');
+  var connectionString = argv.indexOf('--connection-string');
+
+  if (~c) {
+    mongoose.connect(argv[c + 1]);
+  } else if (~connectionString) {
+    mongoose.connect(argv[connectionString + 1]);
+  } else {
+    mongoose.connect(credentials.db);
+  }
+})();
 
 /**
  * Sets mongoose debug mode.
  *
- * To set debug mode pass `true` as third `argv` argument.
+ * To set debug mode pass `--debug` as third `argv` argument.
  *
  * Example:
- * `node app.js true`
+ * `node app.js --debug`
  */
 
-mongoose.set('debug', process.argv[2] || false);
+mongoose.set('debug', (process.argv[2] && process.argv[2] === '--debug') || false);
 
 /**
  * Expose mongoose.
