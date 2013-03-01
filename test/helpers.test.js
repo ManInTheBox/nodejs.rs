10e0589d61d9ba6a527a4b03091eff02f181edaa
Zarko Stankovic <stankovic.zarko@gmail.com>
2013-03-02 00:42:40 +0100

    sredjeni testovi

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -1,10 +1,17 @@
 var helpers = require('../helpers'),
-  fs = require('fs');
+    fs = require('fs')
+  , fixtures = __dirname + '/fixtures';
 
-describe('#markdown()', function () {
-  var text = fs.readFileSync(dirname + '/fixtures/markdown');
-  var r = helpers.markdown(text);
-  it('should parse markdown', function () {
-    r.should.eql('<pre><code class="javasript">ovo')
-  });
+describe('helpers', function () {
+
+    describe('#markdown()', function () {
+        var text = fs.readFileSync(fixtures + '/markdown', 'utf8');
+        var r = helpers.markdown(text);
+
+        it('should parse markdown', function () {
+            r.should.eql("<pre><code class=\"javascript\">alert(&#39;ovo je javascript&#39;);</code></pre>\n");
+        });
+
+    });
+
 });
